cfd38e0e4912f938b7d6711fdbd0d06eb14b606c
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-30 19:03:02 +0800

    feat: add NavLinks to each navbar item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './core/Navbar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<div>Hello World!</div>} />
+      </Routes>
     </>
   );
 }
