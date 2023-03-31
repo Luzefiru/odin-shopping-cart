634c4bd0c82d1a39c3eafb41e56fd89fe2475a59
Luzefiru <ian.deejay.iv@gmail.com>
2023-03-31 11:29:56 +0800

    feat: add home page over-the-fold

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,14 @@
 import './App.css';
-import Navbar from './core/Navbar';
 import { Routes, Route } from 'react-router-dom';
+import Navbar from './core/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<div>Hello World!</div>} />
+        <Route path="/" element={<Home />} />
         <Route path="/catalog" element={<div>Catalog</div>} />
         <Route path="/about" element={<div>About</div>} />
         <Route path="/contact" element={<div>Contact</div>} />
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,19 @@
+import './Home.css';
+import HeaderImage from '../../assets/header-background.jpg';
+
+const Home = () => {
+  return (
+    <>
+      <header>
+        <img className="header__img" alt="Header" src={HeaderImage}></img>
+        <section className="header__cta">
+          <div className="header__cta__caption">
+            “experience the height of fashion„
+          </div>
+        </section>{' '}
+      </header>
+    </>
+  );
+};
+
+export default Home;
