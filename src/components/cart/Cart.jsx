a831ecd09c9da92aed407b0c8488b7d81456affa
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-02 16:18:50 +0800

    feat: add Cart semantics

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -14,6 +14,25 @@ const Cart = () => {
           d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"
         />
       </svg>
+
+      <div className="Cart__header">
+        Your Bag
+        <br />
+        <span className="Cart__header__subtext">You have x items.</span>
+      </div>
+
+      <div className="Cart__checkout">
+        <div className="Cart__checkout__summary">
+          <div className="Cart__checkout__summary__subtotal-title">
+            Subtotal
+          </div>
+          <div className="Cart__checkout__summary__subtotal-price">$1999</div>
+        </div>
+
+        <button type="button" className="Cart__checkout__button">
+          Checkout
+        </button>
+      </div>
     </div>
   );
 };
